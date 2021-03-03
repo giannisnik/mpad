6aeb8edcabc6d33232fbd33355cdb106b28d55a6
Niels van der Heijden <nvanderheijden@deloitte.nl>
2021-03-03 07:26:10 -0800

    progress

--- mpad/learner.py
+++ mpad/learner.py
@@ -1,9 +1,101 @@
-
-
+from models import MPAD
+import torch
+from torch import optim
+from tqdm import tqdm
+import numpy as np
 
 class Learner:
 
-	def __init__(self):
+	def __init__(self, experiment_name, device):
 
 		self.model = None
+		self.optimizer = None
+		self.scheduler = None
+		self.device = device
 		self.writer = None
+		self.train_step = 0
+
+	def init_model(self,
+				   model_type='mpad',
+				   lr=0.1,
+				   **kwargs
+				   ):
+		if model_type.lower() == 'mpad':
+			self.model = MPAD(**kwargs)
+		else:
+			raise AssertionError("Currently only MPAD is supported as model")
+
+		self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
+		self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=50, gamma=0.5)
+
+
+	def train_epoch(self, dataloader, eval_every):
+
+		self.model.train()
+		total_iters = -1
+
+		with tqdm(initial=0, total=eval_every) as pbar_train:
+			for batch_ix, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
+				total_iters += 1
+
+				batch = (t.to(self.device) for t in batch)
+				A, nodes, y = batch
+
+				preds = self.model(nodes, A)
+
+				loss = self.criterion(preds, y)
+
+				self.optimizer.zero_grad()
+				loss.backward()
+
+				# grad norm clipping?
+				self.optimizer.step()
+				self.scheduler.step()
+
+				pbar_train.update(1)
+				pbar_train.set_description(
+					"finetuning phase {} -> loss: {}".format(
+						total_iters + 1, loss.item()
+					)
+				)
+
+				if (total_iters + 1) % eval_every == 0:
+					# Stop training
+					break
+
+
+
+	def evaluate(self, dataloader):
+
+		self.model.eval()
+		y_pred = []
+		y_true = []
+		running_loss = 0
+
+		######################################
+		# Infer the model on the dataset
+		######################################
+		with tqdm(initial=0, total=len(dataloader)) as pbar_eval:
+			with torch.no_grad():
+				for batch_idx, batch in enumerate(dataloader):
+					batch = (t.to(self.device) for t in batch)
+					A, nodes, y = batch
+
+					preds = self.model(nodes, A)
+					loss = self.criterion(preds, y)
+					running_loss += loss.item()
+					# store predictions and targets
+					y_pred.extend(list(preds.cpu().detach().numpy()))
+					y_true.extend(list(np.round(y.cpu().detach().numpy())))
+
+					pbar_eval.update(1)
+					pbar_eval.set_description(
+						"Eval step {} -> loss: {}".format(
+							batch_idx + 1, loss.item()
+						)
+					)
+		######################################
+		# Compute metrics
+		######################################
+
+
--- mpad/main.py
+++ mpad/main.py
@@ -16,11 +16,15 @@ from models import MPAD
 from parser_utils import get_args
 from preprocess_docs import CorpusPreProcessor
 from dataloader import DocumentGraphDataset
+from learner import Learner
 
 if __name__ == "__main__":
 
     args, device = get_args()
 
+    ######################################
+    # Instantiate dataset
+    ######################################
     corpus_prepper = CorpusPreProcessor(
         min_freq_word=1,
         multi_label=False
@@ -49,6 +53,39 @@ if __name__ == "__main__":
         drop_last=True
     )
 
+    ######################################
+    # Istantiate model
+    ######################################
+
+    learner = Learner(
+
+    )
+    learner.init_model(
+        'mpad',
+        lr=args.lr,
+        n_feat=embeddings.shape[1],
+        n_message_passing=args.message_passing_layers,
+        n_hid=args.hidden,
+        n_penultimate=args.penultimate,
+        n_class=n_labels,
+        dropout=args.dropout,
+        embeddings=embeddings,
+        use_master_node=args.use_master_node
+
+    )
+
+    eval_every = len(dataloader_train) if args.eval_every == 'epoch' else int(args.eval_every)
+
+    for epoch in range(args.epochs):
+
+        learner.train_epoch(
+            dataloader_train,
+            eval_every=eval_every
+        )
+
+        learner.evaluate(
+            None
+        )
     # adj, features, _ = create_gows(docs, vocab, args.window_size, args.directed, args.normalize, args.use_master_node)
     #
     # kf = KFold(n_splits=10, shuffle=True)
@@ -86,109 +123,109 @@ if __name__ == "__main__":
     #     n_test_batches = ceil(n_test/args.batch_size)
 
         # Model and optimizer
-        model = MPAD(embeddings.shape[1], args.message_passing_layers, args.hidden, args.penultimate, nclass, args.dropout, embeddings, args.use_master_node)
-
-        parameters = filter(lambda p: p.requires_grad, model.parameters())
-        optimizer = optim.Adam(parameters, lr=args.lr)
-        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.5)
-
-        if args.cuda:
-            model.cuda()
-            adj_train = [x.cuda() for x in adj_train]
-            features_train = [x.cuda() for x in features_train]
-            batch_n_graphs_train = [x.cuda() for x in batch_n_graphs_train]
-            y_train = [x.cuda() for x in y_train]
-            adj_val = [x.cuda() for x in adj_val]
-            features_val = [x.cuda() for x in features_val]
-            batch_n_graphs_val = [x.cuda() for x in batch_n_graphs_val]
-            y_val = [x.cuda() for x in y_val]
-            adj_test = [x.cuda() for x in adj_test]
-            features_test = [x.cuda() for x in features_test]
-            batch_n_graphs_test = [x.cuda() for x in batch_n_graphs_test]
-            y_test = [x.cuda() for x in y_test]
-
-        def train(epoch, adj, features, batch_n_graphs, y):
-            optimizer.zero_grad()
-            output = model(features, adj, batch_n_graphs)
-            loss_train = F.cross_entropy(output, y)
-            loss_train.backward()
-            optimizer.step()
-            return output, loss_train
-
-        def test(adj, features, batch_n_graphs, y):
-            output = model(features, adj, batch_n_graphs)
-            loss_test = F.cross_entropy(output, y)
-            return output, loss_test
-
-        best_acc = 0
-
-        for epoch in range(args.epochs):
-            scheduler.step()
-
-            start = time.time()
-            model.train()
-            train_loss = AverageMeter()
-            train_acc = AverageMeter()
-
-            # Train for one epoch
-            for i in range(n_train_batches):
-                output, loss = train(epoch, adj_train[i], features_train[i], batch_n_graphs_train[i], y_train[i])
-                train_loss.update(loss.item(), output.size(0))
-                train_acc.update(accuracy(output.data, y_train[i].data), output.size(0))
-
-            # Evaluate on validation set
-            model.eval()
-            val_loss = AverageMeter()
-            val_acc = AverageMeter()
-
-            for i in range(n_val_batches):
-                output, loss = test(adj_val[i], features_val[i], batch_n_graphs_val[i], y_val[i])
-                val_loss.update(loss.item(), output.size(0))
-                val_acc.update(accuracy(output.data, y_val[i].data), output.size(0))
-
-            # Print results
-            print("Cross-val iter:", '%02d' % it, "epoch:", '%03d' % (epoch + 1), "train_loss=", "{:.5f}".format(train_loss.avg),
-                "train_acc=", "{:.5f}".format(train_acc.avg), "val_loss=", "{:.5f}".format(val_loss.avg),
-                "val_acc=", "{:.5f}".format(val_acc.avg), "time=", "{:.5f}".format(time.time() - start))
-
-            # Remember best accuracy and save checkpoint
-            is_best = val_acc.avg >= best_acc
-            best_acc = max(val_acc.avg, best_acc)
-            if is_best:
-                early_stopping_counter = 0
-                torch.save({
-                    'epoch': epoch + 1,
-                    'state_dict': model.state_dict(),
-                    'optimizer' : optimizer.state_dict(),
-                }, 'model_best.pth.tar')
-            else:
-                early_stopping_counter += 1
-                print("EarlyStopping: %i / %i" % (early_stopping_counter, args.patience))
-                if early_stopping_counter == args.patience:
-                    print("EarlyStopping: Stop training")
-                    break
-
-        print("Optimization finished!")
-
-        # Testing
-        test_loss = AverageMeter()
-        test_acc = AverageMeter()
-        print("Loading checkpoint!")
-        checkpoint = torch.load('model_best.pth.tar')
-        epoch = checkpoint['epoch']
-        model.load_state_dict(checkpoint['state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer'])
-
-        for i in range(n_test_batches):
-            output, loss = test(adj_test[i], features_test[i], batch_n_graphs_test[i], y_test[i])
-            test_loss.update(loss.item(), output.size(0))
-            test_acc.update(accuracy(output.data, y_test[i].data), output.size(0))
-        accs.append(test_acc.avg.cpu().numpy())
-
-        # Print results
-        print("test_loss=", "{:.5f}".format(test_loss.avg), "test_acc=", "{:.5f}".format(test_acc.avg))
-        print()
-    print("avg_test_acc=", "{:.5f}".format(np.mean(accs)))
+    #     model = MPAD(embeddings.shape[1], args.message_passing_layers, args.hidden, args.penultimate, n_labels, args.dropout, embeddings, args.use_master_node)
+    #
+    #     parameters = filter(lambda p: p.requires_grad, model.parameters())
+    #     optimizer = optim.Adam(parameters, lr=args.lr)
+    #     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.5)
+    #
+    #     if args.cuda:
+    #         model.cuda()
+    #         adj_train = [x.cuda() for x in adj_train]
+    #         features_train = [x.cuda() for x in features_train]
+    #         batch_n_graphs_train = [x.cuda() for x in batch_n_graphs_train]
+    #         y_train = [x.cuda() for x in y_train]
+    #         adj_val = [x.cuda() for x in adj_val]
+    #         features_val = [x.cuda() for x in features_val]
+    #         batch_n_graphs_val = [x.cuda() for x in batch_n_graphs_val]
+    #         y_val = [x.cuda() for x in y_val]
+    #         adj_test = [x.cuda() for x in adj_test]
+    #         features_test = [x.cuda() for x in features_test]
+    #         batch_n_graphs_test = [x.cuda() for x in batch_n_graphs_test]
+    #         y_test = [x.cuda() for x in y_test]
+    #
+    #     def train(epoch, adj, features, batch_n_graphs, y):
+    #         optimizer.zero_grad()
+    #         output = model(features, adj, batch_n_graphs)
+    #         loss_train = F.cross_entropy(output, y)
+    #         loss_train.backward()
+    #         optimizer.step()
+    #         return output, loss_train
+    #
+    #     def test(adj, features, batch_n_graphs, y):
+    #         output = model(features, adj, batch_n_graphs)
+    #         loss_test = F.cross_entropy(output, y)
+    #         return output, loss_test
+    #
+    #     best_acc = 0
+    #
+    #     for epoch in range(args.epochs):
+    #         scheduler.step()
+    #
+    #         start = time.time()
+    #         model.train()
+    #         train_loss = AverageMeter()
+    #         train_acc = AverageMeter()
+    #
+    #         # Train for one epoch
+    #         for i in range(n_train_batches):
+    #             output, loss = train(epoch, adj_train[i], features_train[i], batch_n_graphs_train[i], y_train[i])
+    #             train_loss.update(loss.item(), output.size(0))
+    #             train_acc.update(accuracy(output.data, y_train[i].data), output.size(0))
+    #
+    #         # Evaluate on validation set
+    #         model.eval()
+    #         val_loss = AverageMeter()
+    #         val_acc = AverageMeter()
+    #
+    #         for i in range(n_val_batches):
+    #             output, loss = test(adj_val[i], features_val[i], batch_n_graphs_val[i], y_val[i])
+    #             val_loss.update(loss.item(), output.size(0))
+    #             val_acc.update(accuracy(output.data, y_val[i].data), output.size(0))
+    #
+    #         # Print results
+    #         print("Cross-val iter:", '%02d' % it, "epoch:", '%03d' % (epoch + 1), "train_loss=", "{:.5f}".format(train_loss.avg),
+    #             "train_acc=", "{:.5f}".format(train_acc.avg), "val_loss=", "{:.5f}".format(val_loss.avg),
+    #             "val_acc=", "{:.5f}".format(val_acc.avg), "time=", "{:.5f}".format(time.time() - start))
+    #
+    #         # Remember best accuracy and save checkpoint
+    #         is_best = val_acc.avg >= best_acc
+    #         best_acc = max(val_acc.avg, best_acc)
+    #         if is_best:
+    #             early_stopping_counter = 0
+    #             torch.save({
+    #                 'epoch': epoch + 1,
+    #                 'state_dict': model.state_dict(),
+    #                 'optimizer' : optimizer.state_dict(),
+    #             }, 'model_best.pth.tar')
+    #         else:
+    #             early_stopping_counter += 1
+    #             print("EarlyStopping: %i / %i" % (early_stopping_counter, args.patience))
+    #             if early_stopping_counter == args.patience:
+    #                 print("EarlyStopping: Stop training")
+    #                 break
+    #
+    #     print("Optimization finished!")
+    #
+    #     # Testing
+    #     test_loss = AverageMeter()
+    #     test_acc = AverageMeter()
+    #     print("Loading checkpoint!")
+    #     checkpoint = torch.load('model_best.pth.tar')
+    #     epoch = checkpoint['epoch']
+    #     model.load_state_dict(checkpoint['state_dict'])
+    #     optimizer.load_state_dict(checkpoint['optimizer'])
+    #
+    #     for i in range(n_test_batches):
+    #         output, loss = test(adj_test[i], features_test[i], batch_n_graphs_test[i], y_test[i])
+    #         test_loss.update(loss.item(), output.size(0))
+    #         test_acc.update(accuracy(output.data, y_test[i].data), output.size(0))
+    #     accs.append(test_acc.avg.cpu().numpy())
+    #
+    #     # Print results
+    #     print("test_loss=", "{:.5f}".format(test_loss.avg), "test_acc=", "{:.5f}".format(test_acc.avg))
+    #     print()
+    # print("avg_test_acc=", "{:.5f}".format(np.mean(accs)))
 
 
 
